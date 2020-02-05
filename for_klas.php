ad11bda454af869825c815b9152f48174c058bac
daan <2087884@talnet.nl>
2020-02-05 12:07:29 +0100

    for_klas af

---
+++ for_klas.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $namen = array("Sjonnie de Wiel", "Herman Kaal", "Henk de Steen", "Inge Kerkhoven", "Gert Kruiswijk");
+    $veel = count($namen);
+
+    for ($i = 0; $i <= $veel; $i++){
+        echo($namen[0 + $i]."\n");
+    }
+?>
